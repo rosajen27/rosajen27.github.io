8347aa4172f848f0f8f3d2947f2617f18ff6f0b9
Jennifer Rosa <rosajen27@gmail.com>
2021-02-23 18:00:06 -0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.section-title', { delay: 300 });
+ScrollReveal().reveal('.hero-title', { delay: 500 });
+ScrollReveal().reveal('.hero-cta', { delay: 1000 });
+ScrollReveal().reveal('.about-wrapper .image', { delay: 600 });
+ScrollReveal().reveal('.about-wrapper .info', { delay: 1000 });
+ScrollReveal().reveal('.project-wrapper .text', { delay: 500 });
+ScrollReveal().reveal('.project-wrapper .image', { delay: 1000 });
+ScrollReveal().reveal('.contact-wrapper', { delay: 800 });
